b820e15b488aa3333a0d7d3d3eb83511b45f0ff6
Ed <citylight2392@gmail.com>
2021-08-25 13:00:58 -0500

    1th commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("open");
+    document.querySelector("#burger-span1").classList.toggle("open");
+    document.querySelector("#burger-span2").classList.toggle("open");
+    document.querySelector("#burger-span3").classList.toggle("open");
+}
